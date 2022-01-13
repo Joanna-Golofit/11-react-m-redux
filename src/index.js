c10590df48ed2af32787a6d40fb0b4b64e9b85da
Joanna-Golofit <sunderlajsa@gmail.com>
2022-01-13 15:16:04 +0100

    zakomentowanie nieuzywanych importow

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-import { createStore } from "redux";
+// import { createStore } from "redux";
 import { Provider } from "react-redux";
 import counterReducer from "./reducers/index.js";
 
 
 // cos to nie dziala...
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+// import { combineReducers} from "@reduxjs/toolkit";
 // const reducers = combineReducers({
 //   counterReducer
 // });
